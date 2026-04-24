6af33be0e591bb7183aab79f51fe7a599afd50d0
vansh011206 <vanshajs111@gmail.com>
2026-04-24 09:12:41 +0530

    Day-06-last

---
+++ React-Day-06-UseEffect/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const App = () => {
+
+  const [num, setNum] = useState(0)
+  const [num2, setnum2] = useState(10)
+ useEffect( function(){
+  console.log("useEffect called")
+ },[num])
+ 
+  return (
+    <div>
+      <h1>{num}</h1>
+      <h1>{num2}</h1>
+      <button 
+      onMouseEnter={() => setNum(num + 1)}
+      onMouseLeave={()=> setnum2(num+10)}
+      >Increment</button>
+    </div>
+  )
+}
+
+export default App;
